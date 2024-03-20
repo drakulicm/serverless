b800116318747c1f8103e1f2c6d5d8b7b3b2b5f0
Marko <markodrakulic@Markos-MacBook-Air-2.local>
2024-03-20 13:01:24 +0100

    test deploy

--- apps/web/api/hello.ts
+++ apps/web/api/hello.ts
@@ -17,10 +17,14 @@ export function GET(request: Request) {
 const getMetadata = (pathname: string) => {};
 
 const getIndexHtml = () => {
-  const filePath = path.join(process.cwd(), "apps/web/.vercel/output");
+  const filePath = path.join(process.cwd(), "");
   // list all files in the directory, even the hidden ones
   const files = fs.readdirSync(filePath);
   console.log("🚀 ~ getIndexHtml ~ files:", files);
   const file = fs.readFileSync(filePath);
   return file;
+};
+
+export const config = {
+  runtime: "edge",
 };
