c245675a3e5c5bcd7a90ceaac2cabd1ca6a7e1e9
Marko <markodrakulic@Markos-MacBook-Air-2.local>
2024-03-20 15:56:24 +0100

    use dswap html

--- apps/web/api/hello.ts
+++ apps/web/api/hello.ts
@@ -1,17 +1,32 @@
 // api/metaTags.js
 
 export default async function handler(req: Request, res) {
-  const url = req.url;
+  const pathname = new URL(req.url).pathname;
+
   const metaTags = `
-    <meta name="description" content="${url}">
-    <meta property="og:title" content="${url}">
+    <meta name="description" content="${pathname}">
+    <meta property="og:title" content="${pathname}">
   `;
 
-  const htmlResponse = await fetch("https://serverless.markodrakulic.dev");
-  const htmlBody = await htmlResponse.text();
+  const htmlBody = await getHtmlFileContent(
+    "https://serverless.markodrakulic.dev"
+  );
 
   const modifiedHtmlBody = htmlBody.replace("</head>", `${metaTags}</head>`);
 
   res.setHeader("Content-Type", "text/html");
   res.send(modifiedHtmlBody);
 }
+
+const getHtmlFileContent = async (
+  indexHtmlFileUrl: string
+): Promise<string> => {
+  try {
+    const htmlResponse = await fetch(indexHtmlFileUrl);
+    const htmlBody = await htmlResponse.text();
+
+    return htmlBody;
+  } catch (error) {
+    throw new Error("Firebase error: Can't read index.html");
+  }
+};
